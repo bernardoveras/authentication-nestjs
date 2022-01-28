61f8aa75491f94c771798c5e33c022796b0a4068
Bernardo Veras <bernardo@vvssistemas.com.br>
2022-01-28 13:56:37 -0300

    Add new NestJS project with Swagger

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  
+  const config = new DocumentBuilder()
+    .setTitle('Authentication API')
+    .setDescription('The Authentication API')
+    .setVersion('1.0')
+    .addTag('authentication')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
