59eb7b1c8feffc42252fa1f5dc5927799a497166
Bernardo Veras <bernardo@vvssistemas.com.br>
2022-01-28 15:12:21 -0300

    CHange user controller

--- src/app/auth/auth.controller.ts
+++ src/app/auth/auth.controller.ts
@@ -1,11 +1,14 @@
-import { Controller, Post, UseGuards } from '@nestjs/common';
+import { Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth.service';
 
 @Controller('api/v1/auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService){}
+
   @UseGuards(AuthGuard('local'))
   @Post('authenticate')
-  async authenticate() {
-    return { success: true };
+  async authenticate(@Req() req:any) {
+    return this.authService.authenticate(req.user);
   }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { compareSync } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UsersService) { }
+  constructor(
+    private readonly userService: UsersService,
+    private readonly jwtService: JwtService
+  ) { }
+
+  async authenticate(user) {
+    const payload = { id: user.id, email: user.email };
+
+    return {
+      token: this.jwtService.sign(payload)
+    };
+  }
+
   async validateUser(email: string, password: string) {
     let user;
 
